319dd2d6ba28fc00990df761cfcb727233aa524a
muhammetfurkankorkmaz <225040043@stu.istinye.edu.tr>
2025-04-26 18:04:28 +0300

    Test Update

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hi, I am a test line :=0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
